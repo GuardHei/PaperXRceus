5df9b88f1fac008a943801c2d853cbd6d4bdd4b1
William Henderson <william_henderson@berkeley.edu>
2022-03-28 18:48:04 -0700

    Update

---
+++ Assets/Scripts/PapermonAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PapermonAI : MonoBehaviour
+{
+
+    public PapermonAIState state = PapermonAIState.IDLE;
+    public GameObject enemy;
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case PapermonAIState.IDLE: IdleMove(); break;
+            case PapermonAIState.PREPARING: PreparingMove(); break;
+            case PapermonAIState.COMBATING: CombatingMove(); break;
+        }
+    }
+
+    private void IdleMove()
+    {
+
+    }
+
+    private void PreparingMove()
+    {
+
+    }
+
+    private void CombatingMove()
+    {
+
+    }
+}
+
+public enum PapermonAIState
+{
+    IDLE,
+    PREPARING,
+    COMBATING
+}
---
+++ Assets/Scripts/SelfRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotator : MonoBehaviour
+{
+    public float speed;
+    public Vector3 offset;
+    public Vector3 axis;
+
+    private void Update()
+    {
+        transform.RotateAround(transform.position + offset, axis, speed * Time.deltaTime);
+    }
+}

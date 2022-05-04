adc5c8595f42f96cc7f7566a7ed4da29c275f12e
william-b-henderson <william_henderson@berkeley.edu>
2022-05-03 17:49:02 -0700

    Add tracking for camera to follow player around level

---
+++ Assets/Scripts/TrackPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackPlayer : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player)
+        {
+            transform.position = transform.position + new Vector3(player.transform.position.x - transform.position.x, 0f, 0f);
+        }
+    }
+}

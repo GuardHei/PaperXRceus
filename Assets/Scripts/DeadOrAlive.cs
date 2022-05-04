cd808201dd437cca74bacdedb6e793f635e25a01
william-b-henderson <william_henderson@berkeley.edu>
2022-05-04 14:46:55 -0700

    Add 'You Win' message after defeating all three enemy papermons

---
+++ Assets/Scripts/DeadOrAlive.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadOrAlive : MonoBehaviour
+{
+    public GameObject winText;
+
+    void OnDestroy()
+    {
+        var wt = winText.GetComponent<WinCondition>();
+        wt.updateNumAlive();
+    }
+}
---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    public int numAlive = 3;
+    public bool hasWon = false;
+    public TextMesh text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasWon)
+        {
+            text.text = "You Win!";
+        }
+    }
+
+    public void updateNumAlive()
+    {
+        numAlive -= 1;
+        if (numAlive == 0) hasWon = true;
+    }
+}
